8f11e1d36ef6107abe6e9fd382e5d5d2598631d9
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-06-15 16:09:21 +0300

    ICL: Loggable trait initial.

--- src/Loggable.php
+++ src/Loggable.php
@@ -2,6 +2,20 @@
 
 namespace Illuminated\Console;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 trait Loggable
 {
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->initializeLogging();
+
+        parent::initialize($input, $output);
+    }
+
+    private function initializeLogging()
+    {
+        dd('logging');
+    }
 }

3b24c56bd35d9315bbefc7a7761bbf81fa858c96
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-06-15 16:59:29 +0300

    ICL: Monolog dependency added, Loggable trait continued.

--- src/Loggable.php
+++ src/Loggable.php
@@ -2,20 +2,23 @@
 
 namespace Illuminated\Console;
 
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 trait Loggable
 {
-    protected function initialize(InputInterface $input, OutputInterface $output)
+    public function run(InputInterface $input, OutputInterface $output)
     {
-        $this->initializeLogging();
+        // $type = $this->type();
+        // $environment = app()->environment();
+        // $path = storage_path("logs/cloud/{$type}/{$this->entity}/date.log");
+        //
+        // $handler = new RotatingFileHandler($path, 30);
+        // $handler->setFilenameFormat('{date}', 'Y-m-d');
+        // $log = new Logger($environment, [$handler]);
 
-        parent::initialize($input, $output);
-    }
-
-    private function initializeLogging()
-    {
-        dd('logging');
+        return parent::run($input, $output);
     }
 }

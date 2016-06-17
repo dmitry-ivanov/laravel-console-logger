143f25517972b64878be2fe3d58035c72a2f1302
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-06-17 18:25:40 +0300

    ICL: Helper function removed.

--- src/Log/Formatter.php
+++ src/Log/Formatter.php
@@ -31,21 +31,3 @@ class Formatter extends LineFormatter
         return var_export($data, true);
     }
 }
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function is_json($string, $return = false)
-{
-    if (!is_string($string)) {
-        return false;
-    }
-
-    $data = json_decode($string);
-    if (json_last_error() != JSON_ERROR_NONE) {
-        return false;
-    }
-
-    return ($return ? $data : true);
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

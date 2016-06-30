1df987cbea04dd7300b940b5d6e84c7eb09eb2f0
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-06-30 15:55:46 +0300

    ICL: HtmlFormatter continued.

--- src/Log/HtmlFormatter.php
+++ src/Log/HtmlFormatter.php
@@ -8,15 +8,17 @@ class HtmlFormatter extends MonologHtmlFormatter
 {
     public function format(array $record)
     {
-        $output = $this->composeStyle($record['level']);
-        $output .= $this->composeTitle($record['level_name']);
+        $output = $this->composeStyle($record);
+        $output .= $this->composeTitle($record);
         $output .= $this->composeDetails($record);
 
         return $output;
     }
 
-    protected function composeStyle($level)
+    protected function composeStyle(array $record)
     {
+        $level = $record['level'];
+
         return "<style>
                     .title, .subtitle {
                         background: {$this->logLevels[$level]};
@@ -27,17 +29,19 @@ class HtmlFormatter extends MonologHtmlFormatter
                 </style>";
     }
 
-    protected function composeTitle($value)
+    protected function composeTitle(array $record)
     {
-        $title = "<h2 class='title'>{e($value)}</h2>";
+        $levelName = e($record['level_name']);
+        $title = "<h2 class='title'>{$levelName}</h2>";
 
         $environment = app()->environment();
         if ($environment == 'production') {
             return $title;
         }
 
+        $environment = e($environment);
         $title .= '<style>.title { padding-bottom: 0px !important; } .subtitle { padding-top: 0px !important; }</style>';
-        $title .= "<h3 class='subtitle'>This notification was sent from `{e($environment)}` environment!</h3>";
+        $title .= "<h3 class='subtitle'>This notification was sent from `{$environment}` environment!</h3>";
 
         return $title;
     }

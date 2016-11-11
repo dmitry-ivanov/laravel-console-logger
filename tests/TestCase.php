b885eb422857ff48139697307e7b13eacb3e0046
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-11 12:47:23 +0200

    ICL: Storage setup added.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,22 @@ use Illuminate\Contracts\Console\Kernel as KernelContract;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    protected function getEnvironmentSetUp($app)
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpDatabase();
+        $this->setUpStorage();
+    }
+
+    private function setUpDatabase()
     {
         config(['database.default' => 'testing']);
+    }
+
+    private function setUpStorage()
+    {
+        $this->app->useStoragePath(__DIR__ . '/fixture/storage');
     }
 
     protected function resolveApplicationConsoleKernel($app)

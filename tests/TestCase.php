9a7aba1d3de07f89570fc1b95d9a73cecce17e0c
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-11 13:24:26 +0200

    ICL: Clean logs after tests added.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Contracts\Console\Kernel as KernelContract;
+use Symfony\Component\Finder\Finder;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -25,5 +26,24 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function resolveApplicationConsoleKernel($app)
     {
         $app->singleton(KernelContract::class, Kernel::class);
+    }
+
+    protected function tearDown()
+    {
+        $this->cleanLogsDirectory();
+
+        parent::tearDown();
+    }
+
+    private function cleanLogsDirectory()
+    {
+        $objects = (new Finder)->in(storage_path('logs'))->depth(0);
+        foreach ($objects as $object) {
+            if (File::isDirectory($object)) {
+                File::deleteDirectory($object);
+            } else {
+                File::delete($object);
+            }
+        }
     }
 }

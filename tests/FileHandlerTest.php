83be5aa22817071535298bb6fc858264aa3e5152
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-11 18:14:00 +0200

    ICL: File handler tests added.

--- tests/FileHandlerTest.php
+++ tests/FileHandlerTest.php
@@ -2,15 +2,6 @@
 
 class FileHandlerTest extends TestCase
 {
-    private $date;
-
-    protected function setUp()
-    {
-        parent::setUp();
-
-        $this->date = date('Y-m-d');
-    }
-
     /** @test */
     public function it_creates_log_file_according_to_the_command_name_and_current_date()
     {

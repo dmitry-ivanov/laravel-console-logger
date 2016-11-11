9e28ab4a8d4e56eec5f4a1f6240d353f7c0cedb1
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-11 18:14:49 +0200

    ICL: File handler on mysql tests added.

---
+++ tests/FileHandlerOnMysqlTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class FileHandlerOnMysqlTest extends TestCase
+{
+    protected function setUpDatabase()
+    {
+        config([
+            'database.default' => 'mysql',
+            'database.connections.mysql.database' => '',
+        ]);
+    }
+
+    /** @test */
+    public function it_writes_to_log_file_mysql_specific_information_header()
+    {
+        $dbIp = (string) db_mysql_variable('wsrep_node_address');
+        $dbHost = (string) db_mysql_variable('hostname');
+        $dbPort = (string) db_mysql_variable('port');
+        $now = db_mysql_now();
+
+        Artisan::call('generic');
+
+        $this->assertLogFileContains("generic/{$this->date}.log", [
+            "[%datetime%]: [INFO]: Database host: `{$dbHost}`, port: `{$dbPort}`, ip: `{$dbIp}`.",
+            "[%datetime%]: [INFO]: Database date: `{$now}`",
+        ]);
+    }
+}

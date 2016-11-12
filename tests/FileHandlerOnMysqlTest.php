dfc361c60ecc310e840ce5b2125545248d2d19f9
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-12 13:28:25 +0200

    ICL: File handler tests added.

--- tests/FileHandlerOnMysqlTest.php
+++ tests/FileHandlerOnMysqlTest.php
@@ -41,4 +41,22 @@ class FileHandlerOnMysqlTest extends TestCase
             "[%datetime%]: [INFO]: Database date: `{$now}`",
         ]);
     }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function it_writes_to_log_file_information_footer_each_iteration()
+    {
+        $logger = Mockery::mock(Psr\Log\LoggerInterface::class);
+        $logger->shouldReceive('info')->with('/Execution time\: .*? sec\./')->once();
+        $logger->shouldReceive('info')->with('/Memory peak usage\: .*?\./')->once();
+        $logger->shouldReceive('info')->with('%separator%')->once();
+        $logger->shouldReceive('getHandlers')->withNoArgs()->once()->andReturn([]);
+
+        $handler = new Illuminated\Console\ExceptionHandler($this->app);
+        $handler->initialize($logger);
+        $handler->onShutdown();
+    }
 }
--- tests/FileHandlerTest.php
+++ tests/FileHandlerTest.php
@@ -43,4 +43,22 @@ class FileHandlerTest extends TestCase
             'Database date:',
         ]);
     }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function it_writes_to_log_file_information_footer_each_iteration()
+    {
+        $logger = Mockery::mock(Psr\Log\LoggerInterface::class);
+        $logger->shouldReceive('info')->with('/Execution time\: .*? sec\./')->once();
+        $logger->shouldReceive('info')->with('/Memory peak usage\: .*?\./')->once();
+        $logger->shouldReceive('info')->with('%separator%')->once();
+        $logger->shouldReceive('getHandlers')->withNoArgs()->once()->andReturn([]);
+
+        $handler = new Illuminated\Console\ExceptionHandler($this->app);
+        $handler->initialize($logger);
+        $handler->onShutdown();
+    }
 }

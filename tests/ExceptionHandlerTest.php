325a1e8d608642bd74da6fec2db56133539165a6
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-15 19:49:19 +0200

    ICL: Exception handler test added.

--- tests/ExceptionHandlerTest.php
+++ tests/ExceptionHandlerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminated\Console\ExceptionHandler;
+use Illuminated\Console\RuntimeException;
 use Psr\Log\LoggerInterface;
 
 class ExceptionHandlerTest extends TestCase
@@ -18,5 +19,31 @@ class ExceptionHandlerTest extends TestCase
         $handler = new ExceptionHandler($this->app);
         $handler->setLogger($logger);
         $handler->report(new Exception('Test exception', 111));
+    }
+
+    /** @test */
+    public function it_supports_custom_runtime_exception_which_has_optional_context()
+    {
+        $logger = Mockery::mock(LoggerInterface::class);
+        $logger->shouldReceive('error')->with('Test exception with context', Mockery::subset([
+            'code' => 111,
+            'message' => 'Test exception with context',
+            'file' => __FILE__,
+            'context' => [
+                'foo' => 'bar',
+                'baz' => 123,
+                'faz' => true,
+                'daz' => null,
+            ]
+        ]))->once();
+
+        $handler = new ExceptionHandler($this->app);
+        $handler->setLogger($logger);
+        $handler->report(new RuntimeException('Test exception with context', [
+            'foo' => 'bar',
+            'baz' => 123,
+            'faz' => true,
+            'daz' => null,
+        ], 111));
     }
 }

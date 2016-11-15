a0541db2a32bb56c9ca2027a7e2d61a46f69793d
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-15 18:46:12 +0200

    ICL: Exception handler test added.

---
+++ tests/ExceptionHandlerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminated\Console\ExceptionHandler;
+use Psr\Log\LoggerInterface;
+
+class ExceptionHandlerTest extends TestCase
+{
+    /** @test */
+    public function it_logs_an_error_for_all_occurred_application_exceptions()
+    {
+        $logger = Mockery::mock(LoggerInterface::class);
+        $logger->shouldReceive('error')->with('Test exception', Mockery::subset([
+            'code' => 111,
+            'message' => 'Test exception',
+            'file' => __FILE__,
+        ]))->once();
+
+        $handler = new ExceptionHandler($this->app);
+        $handler->initialize($logger);
+        $handler->report(new Exception('Test exception', 111));
+    }
+}

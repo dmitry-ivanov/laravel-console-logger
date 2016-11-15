87feb85f26e34c034629f0c0f53dbf211554082d
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-15 22:57:19 +0200

    ICL: Custom notification model added.

---
+++ tests/fixture/app/MyCustomNotification.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class MyCustomNotification extends Model
+{
+    protected $fillable = [
+        'level', 'level_name', 'message', 'context', 'custom-field-1', 'custom-field-2', 'custom-field-foo'
+    ];
+}

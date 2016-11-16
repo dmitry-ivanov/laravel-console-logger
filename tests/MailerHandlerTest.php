7bb233789aaf62659cd055c679655136e2f86afe
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-16 16:41:19 +0200

    ICL: Mailer tests added.

---
+++ tests/MailerHandlerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Monolog\Handler\SwiftMailerHandler;
+
+class MailerHandlerOnMailDriverTest extends TestCase
+{
+    /** @test */
+    public function it_validates_and_filters_notification_recipients()
+    {
+        $handler = $this->runViaObject(CommandWithInvalidNotificationRecipients::class)->mailerHandler();
+        $this->assertNotInstanceOf(SwiftMailerHandler::class, $handler);
+    }
+}

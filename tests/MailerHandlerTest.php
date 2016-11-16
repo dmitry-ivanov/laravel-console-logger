cca01192189985e3b8b8e3e9070e297b11782ef8
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-16 17:09:36 +0200

    ICL: Mailer handler tests added.

--- tests/MailerHandlerTest.php
+++ tests/MailerHandlerTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminated\Console\Log\HtmlFormatter;
 use Monolog\Handler\SwiftMailerHandler;
+use Monolog\Logger;
 
 class MailerHandlerTest extends TestCase
 {
@@ -9,5 +11,57 @@ class MailerHandlerTest extends TestCase
     {
         $handler = $this->runViaObject(CommandWithInvalidNotificationRecipients::class)->mailerHandler();
         $this->assertNotInstanceOf(SwiftMailerHandler::class, $handler);
+    }
+
+    /** @test */
+    public function it_uses_configured_monolog_swift_mailer_handler_on_mail_driver()
+    {
+        config(['mail.driver' => 'mail']);
+        $handler = $this->runViaObject(CommandWithNotificationRecipients::class)->mailerHandler();
+
+        $this->assertMailerHandlersAreEqual($this->composeSwiftMailerHandler(), $handler);
+    }
+
+    /** @test */
+    public function it_uses_configured_monolog_swift_mailer_handler_on_smtp_driver()
+    {
+        config(['mail.driver' => 'smtp']);
+        $handler = $this->runViaObject(CommandWithNotificationRecipients::class)->mailerHandler();
+
+        $this->assertMailerHandlersAreEqual($this->composeSwiftMailerHandler(), $handler);
+    }
+
+    private function composeSwiftMailerHandler()
+    {
+        $mailer = app('swift.mailer');
+        $message = $mailer->createMessage();
+        $message->setSubject('[TESTING] %level_name% in `command-with-notification-recipients` command');
+        $message->setFrom(to_swiftmailer_emails(['address' => 'no-reply@example.com', 'name' => 'ICLogger Notification']));
+        $message->setTo(to_swiftmailer_emails([
+            ['address' => 'john.doe@example.com', 'name' => 'John Doe'],
+            ['address' => 'jane.smith@example.com', 'name' => 'Jane Smith'],
+        ]));
+        $message->setContentType('text/html');
+        $message->setCharset('utf-8');
+        $handler = new SwiftMailerHandler($mailer, $message, Logger::NOTICE);
+        $handler->setFormatter(new HtmlFormatter());
+
+        return $handler;
+    }
+
+    protected function assertMailerHandlersAreEqual($handler1, $handler2)
+    {
+        $handler1 = $this->normalizeMailerHandlerDump(get_dump($handler1));
+        $handler2 = $this->normalizeMailerHandlerDump(get_dump($handler2));
+        $this->assertEquals($handler1, $handler2);
+    }
+
+    private function normalizeMailerHandlerDump($dump)
+    {
+        $dump = preg_replace('/\{#\d*/', '{', $dump);
+        $dump = preg_replace('/".*?@swift.generated"/', '"normalized"', $dump);
+        $dump = preg_replace('/-_cacheKey: ".*?"/', '-_cacheKey: "normalized"', $dump);
+
+        return $dump;
     }
 }

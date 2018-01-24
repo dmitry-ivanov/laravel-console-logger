0dc06d5f2972b90ce3ce9497538970ec7da3ffdf
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-01-24 15:56:04 +0200

    Mockery ^1.0 new features used.

--- tests/ConsoleLogger/Exceptions/ExceptionHandlerTest.php
+++ tests/ConsoleLogger/Exceptions/ExceptionHandlerTest.php
@@ -14,33 +14,23 @@ class ExceptionHandlerTest extends TestCase
     /** @test */
     public function it_logs_an_error_for_all_occurred_application_notices_warnings_errors_and_exceptions()
     {
-        $logger = Mockery::mock(LoggerInterface::class);
-        $logger->shouldReceive('error')->with('Test exception', Mockery::subset([
-            'code' => 111,
-            'message' => 'Test exception',
-            'file' => __FILE__,
-        ]))->once();
+        $logger = spy(LoggerInterface::class);
 
         $handler = app(ExceptionHandler::class);
         $handler->setLogger($logger);
         $handler->report(new Exception('Test exception', 111));
+
+        $logger->shouldHaveReceived()->error('Test exception', Mockery::subset([
+            'code' => 111,
+            'message' => 'Test exception',
+            'file' => __FILE__,
+        ]));
     }
 
     /** @test */
     public function it_supports_custom_runtime_exception_which_has_ability_to_set_optional_context()
     {
-        $logger = Mockery::mock(LoggerInterface::class);
-        $logger->shouldReceive('error')->with('Test exception with context', Mockery::subset([
-            'code' => 111,
-            'message' => 'Test exception with context',
-            'file' => __FILE__,
-            'context' => [
-                'foo' => 'bar',
-                'baz' => 123,
-                'faz' => true,
-                'daz' => null,
-            ],
-        ]))->once();
+        $logger = spy(LoggerInterface::class);
 
         $handler = app(ExceptionHandler::class);
         $handler->setLogger($logger);
@@ -50,5 +40,17 @@ class ExceptionHandlerTest extends TestCase
             'faz' => true,
             'daz' => null,
         ], 111));
+
+        $logger->shouldHaveReceived()->error('Test exception with context', Mockery::subset([
+            'code' => 111,
+            'message' => 'Test exception with context',
+            'file' => __FILE__,
+            'context' => [
+                'foo' => 'bar',
+                'baz' => 123,
+                'faz' => true,
+                'daz' => null,
+            ],
+        ]));
     }
 }

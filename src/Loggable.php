4566d0d4d76532d3c4717673b6bc24815ecbdaf4
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2022-08-28 17:23:43 +0300

    Fix coverage

--- src/Loggable.php
+++ src/Loggable.php
@@ -128,7 +128,7 @@ trait Loggable
         app()->singleton(ExceptionHandlerContract::class, ExceptionHandler::class);
 
         /** @noinspection PhpPossiblePolymorphicInvocationInspection */
-        app(ExceptionHandlerContract::class)->initialize($this->icLogger);
+        app(ExceptionHandlerContract::class)->initialize($this->icLogger());
     }
 
     /**
@@ -185,8 +185,6 @@ trait Loggable
 
     /**
      * Get the logger.
-     *
-     * @noinspection PhpUnused
      */
     protected function icLogger(): Logger
     {

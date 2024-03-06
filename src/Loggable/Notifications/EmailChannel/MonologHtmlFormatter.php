bd066607cea46a469ec7b1ede2aff99f4eb523d7
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2024-03-06 22:10:42 +0200

    Formatted

--- src/Loggable/Notifications/EmailChannel/MonologHtmlFormatter.php
+++ src/Loggable/Notifications/EmailChannel/MonologHtmlFormatter.php
@@ -47,13 +47,13 @@ class MonologHtmlFormatter extends HtmlFormatter
     public function getLevelColor(Level $level): string
     {
         return match ($level) {
-            Level::Debug     => '#CCCCCC',
-            Level::Info      => '#28A745',
-            Level::Notice    => '#17A2B8',
-            Level::Warning   => '#FFC107',
-            Level::Error     => '#FD7E14',
-            Level::Critical  => '#DC3545',
-            Level::Alert     => '#821722',
+            Level::Debug => '#CCCCCC',
+            Level::Info => '#28A745',
+            Level::Notice => '#17A2B8',
+            Level::Warning => '#FFC107',
+            Level::Error => '#FD7E14',
+            Level::Critical => '#DC3545',
+            Level::Alert => '#821722',
             Level::Emergency => '#000000',
         };
     }
